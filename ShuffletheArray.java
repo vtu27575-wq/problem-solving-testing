21fd5f0a6de3a6812b1e088bb9422e682d208101
vtu27575-wq <vtu27575@veltech.edu.in>
2026-08-14 10:32:40 +0530

    Update ShuffletheArray.java

--- ShuffletheArray.java
+++ ShuffletheArray.java
@@ -3,8 +3,8 @@ class Solution {
         int[] ans = new int[2 * n];
 
         for (int i = 0; i < n; i++) {
-            ans[2 * i] = nums[i];       // x
-            ans[2 * i + 1] = nums[i + n]; // y
+            ans[2 * i] = nums[i];       
+            ans[2 * i + 1] = nums[i + n]; 
         }
 
         return ans;
