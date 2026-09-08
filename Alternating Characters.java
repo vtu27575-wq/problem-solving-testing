4309f30d2139a07274dcc52fc64b24ae23d3453f
vtu27575-wq <vtu27575@veltech.edu.in>
2026-09-08 22:45:48 +0530

    Implement alternatingCharacters function

---
+++ Alternating Characters.java
@@ -0,0 +1,11 @@
+class Result {
+    public static int alternatingCharacters(String s) {
+        int count = 0;
+
+        for (int i = 1; i < s.length(); i++)
+            if (s.charAt(i) == s.charAt(i - 1))
+                count++;
+
+        return count;
+    }
+}
