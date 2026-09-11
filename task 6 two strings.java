eba37ddd1dc608c338fb6530b62a6e11ed29ecab
vtu27575-wq <vtu27575@veltech.edu.in>
2026-09-11 09:45:30 +0530

    Create task 6 two strings.java

---
+++ task 6 two strings.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public static String twoStrings(String s1, String s2) {
+        boolean[] seen = new boolean[26];
+
+        for (char c : s1.toCharArray()) {
+            seen[c - 'a'] = true;
+        }
+
+        for (char c : s2.toCharArray()) {
+            if (seen[c - 'a']) {
+                return "YES";
+            }
+        }
+
+        return "NO";
+    }
+}
